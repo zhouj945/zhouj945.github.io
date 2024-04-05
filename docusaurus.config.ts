49dbb9cd63276f6ebbf8fb737028cb6355799e2a
zhouj945 <zhou_j@163.com>
2024-04-06 02:26:12 +0800

    feat: init project

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: `Damon's Site`,
+  title: `Damon's Site!`,
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
